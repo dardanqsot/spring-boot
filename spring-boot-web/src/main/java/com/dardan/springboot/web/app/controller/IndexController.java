87bb477c7dc39b4baa76636985edcceec9cdcea5
Dardan <quispesotodaniel@gmail.com>
2021-09-13 23:40:07 -0500

    Seccion 2 Spring MVC: Primer controller y vista.

---
+++ spring-boot-web/src/main/java/com/dardan/springboot/web/app/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.dardan.springboot.web.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    //@RequestMapping(value="/index",method= RequestMethod.GET)
+    //@GetMapping(value="/index")
+    //@GetMapping("/index")
+    @GetMapping({"/index", "/", "/home"})
+    public String index() {
+        return "index"; // tiene q existir una vista con ese nombre
+    }
+}
