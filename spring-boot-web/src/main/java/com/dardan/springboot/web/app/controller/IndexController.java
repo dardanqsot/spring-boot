75e89f2f3c1dc17e4074d04371aef884473088bb
Dardan <quispesotodaniel@gmail.com>
2021-09-14 23:34:17 -0500

    Seccion 2 Spring MVC: Pasar datos a vista y uso de @RequestMapping

--- spring-boot-web/src/main/java/com/dardan/springboot/web/app/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/dardan/springboot/web/app/controller/IndexController.java
@@ -1,18 +1,38 @@
 package com.dardan.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
+@RequestMapping("/app") //ruta de primer nivel
 public class IndexController {
 
     //@RequestMapping(value="/index",method= RequestMethod.GET)
     //@GetMapping(value="/index")
     //@GetMapping("/index")
-    @GetMapping({"/index", "/", "/home"})
-    public String index() {
+    @GetMapping({"/index", "/", "", "/home"})
+    public String index(Model model) {
+        model.addAttribute("titulo","Hola Spring Framework con Model");
         return "index"; // tiene q existir una vista con ese nombre
     }
+//  2 da forma
+//    @GetMapping({"/index", "/", "/home"})
+//    public String index(Map<String, Object> map) {
+//        map.put("titulo", "Hola Spring Framework con Map");
+//        return "index"; // tiene q existir una vista con ese nombre
+//    }
+// 3ra forma
+//    @GetMapping({"/index", "/", "/home"})
+//    public ModelAndView index(ModelAndView mv) {
+//        mv.addObject("titulo", "Hola Spring Framework con ModelAndView");
+//        mv.setViewName("index");
+//        return mv; // tiene q existir una vista con ese nombre
+//    }
 }
