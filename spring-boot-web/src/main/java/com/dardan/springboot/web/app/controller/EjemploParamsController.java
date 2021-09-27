b9e50de32145cc5bd7e9e37e363ca6a849ccdcef
darwinQSot <quispesotodaniel@gmail.com>
2021-09-26 23:57:48 -0500

    Seccion 2 Spring MVC : Obtener varios parametros de la URL con @RequeswtParam

--- spring-boot-web/src/main/java/com/dardan/springboot/web/app/controller/EjemploParamsController.java
+++ spring-boot-web/src/main/java/com/dardan/springboot/web/app/controller/EjemploParamsController.java
@@ -19,4 +19,10 @@ public class EjemploParamsController {
         model.addAttribute("resultado","El texto enviado es: " + texto);
         return "params/ver";
     }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model){
+        model.addAttribute("resultado","El saludo es: '" + saludo + "' y el número es: '"+ numero + "'");
+        return "params/ver";
+    }
 }
