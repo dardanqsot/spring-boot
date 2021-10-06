717061f52455500d590a32701ba216137f549f6a
darwinQSot <quispesotodaniel@gmail.com>
2021-10-05 22:39:17 -0500

    Seccion 3 Inyeccion de dependencias : Añadiendo clase de servicio

--- spring-boot-di/src/main/java/com/dardan/springbootdi/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/dardan/springbootdi/app/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.dardan.springbootdi.app.controller;
 
+import com.dardan.springbootdi.app.models.service.MiServicio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private MiServicio servicio = new MiServicio();
+
     @GetMapping({"/","","/index"})
     public String index(Model model) {
+        model.addAttribute("objeto", servicio.operacion());
         return "index";
     }
 }
