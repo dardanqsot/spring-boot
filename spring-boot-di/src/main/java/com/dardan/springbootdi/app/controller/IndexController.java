fc4b68d92a4d2ed02a9d311bdb3bcd5b71fd4721
darwinQSot <quispesotodaniel@gmail.com>
2021-10-05 22:48:24 -0500

    Seccion 3 Inyeccion de dependencias : anotación Autowired

--- spring-boot-di/src/main/java/com/dardan/springbootdi/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/dardan/springbootdi/app/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.dardan.springbootdi.app.controller;
 
 import com.dardan.springbootdi.app.models.service.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    private MiServicio servicio = new MiServicio();
+    @Autowired   //sirve para inyectar un objeto guardado en el contenedor de spring
+    private MiServicio servicio;
 
     @GetMapping({"/","","/index"})
     public String index(Model model) {
--- spring-boot-di/src/main/java/com/dardan/springbootdi/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/dardan/springbootdi/app/models/service/MiServicio.java
@@ -1,8 +1,12 @@
 package com.dardan.springbootdi.app.models.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
 public class MiServicio {
 
     public String operacion() {
-        return "Ejecutando algun proceso importante ...";
+        return "Ejecutando algun proceso importante con service ...";
     }
 }
