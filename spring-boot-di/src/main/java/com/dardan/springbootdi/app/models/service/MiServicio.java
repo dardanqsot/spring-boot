d9a02fdacdf5e8516df14add1ca46bf73adf8741
darwinQSot <quispesotodaniel@gmail.com>
2021-10-05 22:31:37 -0500

    Seccion 3 Inyeccion de dependencias : Creación de proyecto y controlador

---
+++ spring-boot-di/src/main/java/com/dardan/springbootdi/app/models/service/MiServicio.java
@@ -0,0 +1,8 @@
+package com.dardan.springbootdi.app.models.service;
+
+public class MiServicio {
+
+    public String operacion() {
+        return "Ejecutando algun proceso importante ...";
+    }
+}
