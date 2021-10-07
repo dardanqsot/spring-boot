95d40834fc715950762316fc7813433351b79684
darwinQSot <quispesotodaniel@gmail.com>
2021-10-06 21:20:21 -0500

    Seccion 3 Inyeccion de dependencias : Anotación @Qualifier

--- spring-boot-di/src/main/java/com/dardan/springbootdi/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/dardan/springbootdi/app/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.dardan.springbootdi.app.controller;
 
 import com.dardan.springbootdi.app.models.service.IServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @Autowired   // @Autowired sirve para inyectar un objeto guardado en el contenedor de spring
+    @Qualifier("miServicioSimple") // Para indicar un componente específico a utilizar, se puede usar sin el Primary
     private IServicio servicio;
 
     @GetMapping({"/","","/index"})
