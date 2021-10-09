6bac2bc4680193d714c4e8ff79436153d2f02cdd
darwinQSot <quispesotodaniel@gmail.com>
2021-10-09 18:58:34 -0500

    Seccion 3 Inyeccion de dependencias : Ejemplo de usoagregando componentes y usando config para metodos específicos.

--- spring-boot-di/src/main/java/com/dardan/springbootdi/app/AppConfig.java
+++ spring-boot-di/src/main/java/com/dardan/springbootdi/app/AppConfig.java
@@ -1,11 +1,16 @@
 package com.dardan.springbootdi.app;
 
+import com.dardan.springbootdi.app.models.domain.ItemFactura;
+import com.dardan.springbootdi.app.models.domain.Producto;
 import com.dardan.springbootdi.app.models.service.IServicio;
 import com.dardan.springbootdi.app.models.service.MiServicio;
 import com.dardan.springbootdi.app.models.service.MiServicioComplejo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -20,4 +25,16 @@ public class AppConfig {
     public IServicio registrarMiServicioComplejo(){
         return new MiServicioComplejo();
     }
+
+    @Bean("itemFactura")
+    public List<ItemFactura> registrarItems(){
+        Producto producto1 = new Producto("Camara Sony",100);
+        Producto producto2 = new Producto("Bicicleta aro 25",200);
+
+        ItemFactura linea1 = new ItemFactura(producto1, 2);
+        ItemFactura linea2 = new ItemFactura(producto2, 4);
+
+        return Arrays.asList(linea1, linea2);
+    }
 }
+
