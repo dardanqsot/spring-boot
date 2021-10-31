f4f51e4178005189217eaa4a6859609702d6c7cf
darwinQSot <quispesotodaniel@gmail.com>
2021-10-31 12:35:02 -0500

    Seccion 4: Formularios Thymeleaf y Data Binding: Creando clase model del formulario

--- spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.dardan.springboo.form.app.controllers;
 
+import com.dardan.springboo.form.app.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +20,14 @@ public class FormController {
     public String procesar(Model model, @RequestParam(name="username") String username, //se puedo colocar el nombre en caso sean diferentes
                            @RequestParam String password,
                            @RequestParam String email){
+
+        Usuario usuario = new Usuario(); //clase pojo no tiene sentido injectar ya que represanta los datos de la aplicacion a diferencia de un servicio o configuracion, utilidad etc
+        // los datos por lo general no se inyectan se persisten, envian guardan enviar a vista etc, solo los reposito, service, etc
+        usuario.setUsername(username);
+        usuario.setEmail(email);
+        usuario.setPassword(password);
         model.addAttribute("titulo", "Resultado form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("usuario", usuario);
         return "resultado";
     }
 }
