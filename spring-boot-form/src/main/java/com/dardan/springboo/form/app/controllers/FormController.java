d365c65cb6d0f472b2145089239cba2e0f4ab309
darwinQSot <quispesotodaniel@gmail.com>
2021-10-31 12:40:23 -0500

    Seccion 4: Formularios Thymeleaf y Data Binding: Mapeando campos del formulario al objeto model

--- spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -17,15 +16,7 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Model model, @RequestParam(name="username") String username, //se puedo colocar el nombre en caso sean diferentes
-                           @RequestParam String password,
-                           @RequestParam String email){
-
-        Usuario usuario = new Usuario(); //clase pojo no tiene sentido injectar ya que represanta los datos de la aplicacion a diferencia de un servicio o configuracion, utilidad etc
-        // los datos por lo general no se inyectan se persisten, envian guardan enviar a vista etc, solo los reposito, service, etc
-        usuario.setUsername(username);
-        usuario.setEmail(email);
-        usuario.setPassword(password);
+    public String procesar(Usuario usuario, Model model){
         model.addAttribute("titulo", "Resultado form");
         model.addAttribute("usuario", usuario);
         return "resultado";
