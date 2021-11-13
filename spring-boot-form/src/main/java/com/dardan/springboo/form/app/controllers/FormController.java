1c35aece939f7f79d4eff724d4604c97dba23cf9
darwinQSot <quispesotodaniel@gmail.com>
2021-11-13 17:55:09 -0500

    Seccion 4: Formularios Thymeleaf y Data Binding: El atributo object de thymeleaf en el elemento for

--- spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
@@ -5,13 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class FormController {
@@ -20,20 +16,20 @@ public class FormController {
     public String form(Model model) {
         Usuario usuario = new Usuario();
         model.addAttribute("titulo", "Formulario usuarios");
-        model.addAttribute("user", usuario);
+        model.addAttribute("usuario", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model){ //Por defecto se usa el nombnre de la clase , pero con en model Atribute se puede cambiar de nombre
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){ //Por defecto se usa el nombnre de la clase , pero con en model Atribute se puede cambiar de nombre
 
         model.addAttribute("titulo", "Resultado form");
         if(result.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
+/*            Map<String, String> errores = new HashMap<>();
             result.getFieldErrors().forEach(err -> {
                 errores.put(err.getField(), "El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
             });
-            model.addAttribute("error", errores);
+            model.addAttribute("error", errores);*/
             return "form";
         }
 
