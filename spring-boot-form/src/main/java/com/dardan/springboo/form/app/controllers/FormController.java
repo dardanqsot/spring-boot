8166b5936409315a422d7e629ba30d3305330c2d
darwinQSot <quispesotodaniel@gmail.com>
2021-11-13 17:43:38 -0500

    Seccion 4: Formularios Thymeleaf y Data Binding: Añadiendo los mensajes de errores en la vista del formulario

--- spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/dardan/springboo/form/app/controllers/FormController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.naming.Binding;
@@ -17,12 +18,14 @@ public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        Usuario usuario = new Usuario();
         model.addAttribute("titulo", "Formulario usuarios");
+        model.addAttribute("user", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model){ //Por defecto se usa el nombnre de la clase , pero con en model Atribute se puede cambiar de nombre
 
         model.addAttribute("titulo", "Resultado form");
         if(result.hasErrors()){
