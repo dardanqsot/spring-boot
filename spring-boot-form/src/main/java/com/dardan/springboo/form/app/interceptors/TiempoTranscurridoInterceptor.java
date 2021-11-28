468091063936c9147102d13d012a245e745a3eaf
darwinQSot <quispesotodaniel@gmail.com>
2021-11-27 20:06:48 -0500

    Seccion 5: Interceptores HTTP: Registrando interceptor en la configuración de spring

--- spring-boot-form/src/main/java/com/dardan/springboo/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ spring-boot-form/src/main/java/com/dardan/springboo/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Random;
 
-@Component
+@Component("tiempoTranscurridoInterceptor")
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
