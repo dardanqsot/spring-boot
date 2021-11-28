4d9909c8e20751e4ff4b1867359d909b9af57d73
darwinQSot <quispesotodaniel@gmail.com>
2021-11-27 20:18:12 -0500

    Seccion 5: Interceptores HTTP: Implementando Fragment interceptor para reutilizar código en thymeleaf

--- spring-boot-form/src/main/java/com/dardan/springboo/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ spring-boot-form/src/main/java/com/dardan/springboo/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -3,6 +3,7 @@ package com.dardan.springboo.form.app.interceptors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,33 +15,52 @@ import java.util.Random;
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        if (request.getMethod().equalsIgnoreCase("post")) {
+            return true;
+        }
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod metodo = (HandlerMethod) handler;
+            logger.info("es un método del controlador: " + metodo.getMethod().getName());
+        }
 
         logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+        logger.info("Interceptando: " + handler);
         long tiempoInicio = System.currentTimeMillis();
         request.setAttribute("tiempoInicio", tiempoInicio);
 
         Random random = new Random();
-        Integer demora = random.nextInt(500);
-
+        Integer demora = random.nextInt(100);
         Thread.sleep(demora);
+
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+        if (request.getMethod().equalsIgnoreCase("post")) {
+            return;
+        }
+
         long tiempoFin = System.currentTimeMillis();
         long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
         long tiempoTranscurrido = tiempoFin - tiempoInicio;
 
-        if(modelAndView != null) {
+        if (handler instanceof HandlerMethod && modelAndView != null) {
             modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
         }
-
-        logger.info("TiempoTranscurrido: " + tiempoTranscurrido + " milisegundos");
-        logger.info("TiempoTranscurridoInterceptor: preHandle() saliendo ...");
+        logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisegundos");
+        logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo ...");
 
     }
+
+
 }
+
